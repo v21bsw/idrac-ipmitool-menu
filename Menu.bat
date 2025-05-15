c30537fa34e9c2a1fc7cb47eed6088e152b2f928
v21bsw <ben@subcold.uk>
2025-05-15 13:27:18 +0100

    Update Menu.bat

--- Menu.bat
+++ Menu.bat
@@ -5,16 +5,13 @@ COLOR 0A
 :: ================= CONFIGURATION =================
 :: Edit these values to match your friend's servers
 
-::set SERVER1=192.168.50.233
-::set SERVER3=192.168.50.232
-::set SERVER2=192.168.50.231
-::set SERVER4=192.168.50.230
+
 
 
 set SERVER1=192.168.50.230
 set SERVER2=192.168.50.231
-set SERVER3=192.168.50.232
-set SERVER4=192.168.50.233
+set SERVER3=192.168.50.233
+
 
 
 
@@ -29,8 +26,6 @@ set PASSWORD2=admin
 set USERNAME3=admin
 set PASSWORD3=admin
 
-set USERNAME4=admin
-set PASSWORD4=admin
 :: =================================================
 
 :menu
@@ -38,33 +33,55 @@ cls
 echo ====================================
 echo          DELL SERVER FAN CONTROL
 echo ====================================
-echo [1] Set Custom Fan Speed (10%% to 100%%)
-echo [2] Set All Fans to 10%%
-echo [3] Set All Fans to 100%%
-echo [4] Put Fans to Auto Mode
-echo [5] Check Fan Speed
-echo [6] Check Ambient Temperature
-echo [7] Turn OFF Fans
-echo [8] Turn ON All Servers
-echo [9] Turn OFF All Servers (CONFIRMATION REQUIRED)
+echo [1] Check Power
+echo [2] Turn ON All Servers
+echo [3] Turn OFF All Servers (CONFIRMATION REQUIRED)
+echo [4] Check Ambient Temperature
+echo [5] Set Custom Fan Speed (10%% to 100%%)
+echo [6] Set All Fans to 10%%
+echo [7] Set All Fans to 100%%
+echo [8] Put Fans to Auto Mode
+echo [9] Check Fan Speed
+echo [19] Turn OFF Fans
 echo [0] Exit
 echo ====================================
 set /p choice=Select an option (0-9): 
 
-if "%choice%"=="1" goto custom_speed
-if "%choice%"=="2" goto set_10
-if "%choice%"=="3" goto set_100
-if "%choice%"=="4" goto fans_auto
-if "%choice%"=="5" goto check_fans
-if "%choice%"=="6" goto check_temps
-if "%choice%"=="7" goto turn_off_fans
-if "%choice%"=="8" goto turn_on
-if "%choice%"=="9" goto turn_off
-if "%choice%"=="0" exit
+if "%choice%"=="1" goto check_power
+if "%choice%"=="2" goto turn_on
+if "%choice%"=="3" goto turn_off
+if "%choice%"=="4" goto check_temps
+if "%choice%"=="5" goto custom_speed
+if "%choice%"=="6" goto set_10
+if "%choice%"=="7" goto set_100
+if "%choice%"=="8" goto fans_auto
+if "%choice%"=="9" goto check_fans
+if "%choice%"=="10" goto turn_off_fans
+
 
 echo Invalid option. Please enter a valid choice.
 pause
 goto menu
+
+:check_power
+cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
+echo Checking power usage...
+for %%S in (1 3) do (
+    setlocal enabledelayedexpansion
+    set "SERVER_IP=!SERVER%%S!"
+    set "USER=!USERNAME%%S!"
+    set "PASS=!PASSWORD%%S!"
+    echo.
+    echo === Server %%S: !SERVER_IP! ===
+    ipmitool -I lanplus -H !SERVER_IP! -U !USER! -P !PASS! dcmi power reading
+    endlocal
+)
+echo.
+echo Power check completed.
+pause
+goto menu
+
+
 
 
 :rev_servers
@@ -77,7 +94,7 @@ goto apply_speed
 :check_temps
 cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
 echo Checking ambient temperature...
-for %%S in (1 2 3 4) do (
+for %%S in (1 2 3) do (
     setlocal enabledelayedexpansion
     set "SERVER_IP=!SERVER%%S!"
     set "USER=!USERNAME%%S!"
@@ -122,7 +139,7 @@ goto apply_speed
 
 :apply_speed
 cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
-for %%S in (1 2 3 4) do (
+for %%S in (1 2 3) do (
     setlocal enabledelayedexpansion
     set "SERVER_IP=!SERVER%%S!"
     set "USER=!USERNAME%%S!"
@@ -137,7 +154,7 @@ goto menu
 
 :fans_auto
 cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
-for %%S in (1 2 3 4) do (
+for %%S in (1 2 3) do (
     setlocal enabledelayedexpansion
     set "SERVER_IP=!SERVER%%S!"
     set "USER=!USERNAME%%S!"
@@ -152,7 +169,7 @@ goto menu
 :check_fans
 cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
 echo Checking fan speeds...
-for %%S in (1 2 3 4) do (
+for %%S in (1 2 3) do (
     setlocal enabledelayedexpansion
     set "SERVER_IP=!SERVER%%S!"
     set "USER=!USERNAME%%S!"
@@ -170,7 +187,7 @@ goto menu
 cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
 set fan_percent=0
 set fan_hex=0x00
-for %%S in (1 2 3 4) do (
+for %%S in (1 2 3) do (
     setlocal enabledelayedexpansion
     set "SERVER_IP=!SERVER%%S!"
     set "USER=!USERNAME%%S!"
@@ -187,7 +204,7 @@ goto menu
 :turn_on
 cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
 echo Powering ON servers...
-for %%S in (1 2 3 4) do (
+for %%S in (1 2 3) do (
     setlocal enabledelayedexpansion
     set "SERVER_IP=!SERVER%%S!"
     set "USER=!USERNAME%%S!"
@@ -203,7 +220,7 @@ cd "C:\Program Files (x86)\Dell\SysMgt\bmc"
 echo Are you sure you want to SHUT DOWN all servers? (Y/N)
 set /p confirm=
 if /I "%confirm%"=="Y" (
-    for %%S in (1 2 3 4) do (
+    for %%S in (1 2 3) do (
         setlocal enabledelayedexpansion
         set "SERVER_IP=!SERVER%%S!"
         set "USER=!USERNAME%%S!"
